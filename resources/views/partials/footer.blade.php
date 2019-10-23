268b678daa28257a6fc1232f2362d011db4c7041
Guntis Sulcs <guntis@brandbox.digital>
2019-10-23 17:02:31 +0300

    Edited some view file

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,3 +2,8 @@
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>© 2017-2019 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
 </footer>
+
+
+<div class="second-footer">
+    <h1>Yolo!</h1>
+</div>
